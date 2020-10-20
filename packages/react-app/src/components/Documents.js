abdc2169e0e6c30255b253507930848e0e403aff
koushith <koushith97@gmail.com>
2020-10-20 13:53:00 +0530

    adds modal on documents page

--- packages/react-app/src/components/Documents.js
+++ packages/react-app/src/components/Documents.js
@@ -1,17 +1,20 @@
 /* eslint-disable */
 import React, { useEffect, useState } from "react";
-import { Button, Icon, Loader, Table } from "semantic-ui-react";
-const index = require("../lib/e2ee.js");
+import { Button, Icon, Loader, Table, Modal, Header, Form } from "semantic-ui-react";
+//import DocumentModal from "./DocumentModal";
+const index = require("../lib/e2ee");
 
 const userType = { party: 0, notary: 1 };
 
 export default function Documents(props) {
   const password = localStorage.getItem("password");
-
+  const [open, setOpen] = useState(false);
   const [caller, setCaller] = useState({});
   const [signer, setSigner] = useState({});
   const [docs, setDocs] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  // modal
 
   console.log(docs);
 
@@ -118,6 +121,87 @@ export default function Documents(props) {
               Loading
             </Loader>
           )}
+        </Table.Body>
+      </Table>
+
+      {/* demo -replace onclick-modal with actual table data */}
+
+      <Modal onClose={() => setOpen(false)} onOpen={() => setOpen(true)} open={open}>
+        <Modal.Header>Document Status</Modal.Header>
+        <Modal.Content>
+          <Table padded="very">
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell>
+                  <h3>Document Name</h3>
+                </Table.Cell>
+                <Table.Cell>
+                  <h3>Rental Agreement</h3>
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>
+                  <h3>Document Hash</h3>
+                </Table.Cell>
+                <Table.Cell>
+                  <h3>0x337b2aF19e840E8761Ef7a90Ce05Fedf4E91E2B2</h3>
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>
+                  <h3>Created By</h3>
+                </Table.Cell>
+                <Table.Cell>
+                  <h3>Koushith@consensolabs.com</h3>
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>
+                  <h3>Signature Status</h3>
+                </Table.Cell>
+                <Table.Cell>
+                  <Button color="green">Sign Now</Button>
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>
+                  <h3>Notarised?</h3>
+                </Table.Cell>
+                <Table.Cell>
+                  <h3>No</h3>
+                </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color="black" onClick={() => setOpen(false)}>
+            Close
+          </Button>
+        </Modal.Actions>
+      </Modal>
+
+      <Table singleLine striped>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Status</Table.HeaderCell>
+            <Table.HeaderCell>Notes</Table.HeaderCell>
+            <Table.HeaderCell>Actions</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          <Table.Row onClick={() => setOpen(true)}>
+            <Table.Cell>John</Table.Cell>
+            <Table.Cell>Approved</Table.Cell>
+            <Table.Cell>None</Table.Cell>
+            <Table.Cell>
+              <Button className="button__primary" onClick={() => setOpen(true)}>
+                More Info
+              </Button>
+            </Table.Cell>
+          </Table.Row>
         </Table.Body>
       </Table>
     </div>
